b157cf1f46284590cf6af0564f4a71a69bb641a3
Kolya Koval <kovalkola2@gmail.com>
2021-07-02 10:54:05 +0300

    Init

---
+++ ArdillaShop/ArdillaShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArdillaShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
