eb49c88f4712c39f3974037226cbb744d7f637e1
Kolya Koval <kovalkola2@gmail.com>
2021-07-05 19:27:55 +0300

    Add Identity

--- ArdillaShop/ArdillaShop/Data/ApplicationDbContext.cs
+++ ArdillaShop/ArdillaShop/Data/ApplicationDbContext.cs
@@ -4,13 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ArdillaShop.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
         public DbSet<Category> Category { get; set; }
         public DbSet<Product> Product { get; set; }
+        public DbSet<AppUser> AppUser { get; set; }
     }
 }
